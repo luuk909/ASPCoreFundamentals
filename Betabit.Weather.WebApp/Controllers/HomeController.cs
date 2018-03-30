16919943b4ffc18fb789577b2a99c743ea96d63b
Luuk ten Böhmer <luuk.tenbohmer@hotmail.com>
2018-03-30 09:42:27 +0200

    Add project files.

---
+++ Betabit.Weather.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Betabit.Weather.WebApp.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "Home";
+        }
+    }
+}
